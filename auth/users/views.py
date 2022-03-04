e9f49db0c2f5cedf9d6d8b912d7aa8ea2649314c
Carlo Occhiena <robotfood@virgilio.it>
2022-03-04 15:34:56 +0100

    update

--- auth/users/views.py
+++ auth/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def home(request):
+    return render(request, 'users/success.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
---
+++ wip_list.md
@@ -0,0 +1,2 @@
+1. user management https://realpython.com/django-user-management/
+also: https://medium.com/swlh/django-user-authentication-in-5-minutes-4db08c5c459a
